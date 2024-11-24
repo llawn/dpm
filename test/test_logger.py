f30f9a6d5e3988e14ec177e2196cf897b3bae877
llawn <llawn06@gmail.com>
2024-11-24 19:02:07 +0100

    test(logging): test loggers for dpm and test itself

---
+++ test/test_logger.py
@@ -0,0 +1,20 @@
+import unittest
+
+from src.dpm import logger as dpmlogger
+from test import logger as testlogger
+
+
+class TestLoggers(unittest.TestCase):
+    def test_test_logger(self):
+        testlogger.debug("This is a debug message.")
+        testlogger.info("This is an info message.")
+        testlogger.warning("This is a warning message.")
+        testlogger.error("This is an error message.")
+        self.assertTrue(True)  # Simple assertion to make the test pass
+
+    def test_dpm_logger(self):
+        dpmlogger.debug("This is a debug message.")
+        dpmlogger.info("This is an info message.")
+        dpmlogger.warning("This is a warning message.")
+        dpmlogger.error("This is an error message.")
+        self.assertTrue(True)  # Simple assertion to make the test pass
