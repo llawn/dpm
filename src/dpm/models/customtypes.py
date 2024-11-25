bfaa8b880f1cb7685e1c61f2219e61f732e1c614
llawn <llawn06@gmail.com>
2024-11-25 13:43:47 +0100

    docs(types): add documentation for customtypes

--- src/dpm/models/customtypes.py
+++ src/dpm/models/customtypes.py
@@ -2,4 +2,5 @@ from typing import Annotated
 
 from pydantic.types import StringConstraints
 
+# username for User model
 UsernameStr = Annotated[str, StringConstraints(min_length=5, max_length=50)]
